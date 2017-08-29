393b644962b02052dbe336a83967608e1ed7f0a1
lilei <240924085@qq.com>
2017-08-29 16:57:22 +0800

    已完成审批环节、审批方式、人员范围联动

--- pages/oa/backlogApprove.js
+++ pages/oa/backlogApprove.js
@@ -8,11 +8,12 @@ Page({
   data: {
     isCanOpinion: '',
     sphj_index: 0,
-    sphj_id: [],
-    sphj_name: [],
+    sphj: [],
     spfs_index: 0,
-    spfs_id: [],
-    spfs_name: []
+    spfs: [],
+    ryfw_index: 0,
+    ryfw: [],
+    hasDefault: false
   },
   onLoad: function (options) {
     user_code = wx.getStorageSync('userinfo').username
@@ -39,25 +40,19 @@ Page({
     var that = this
     oa.getApproveInfo(user_code, fw_id, bu_code, function(data){
       splist = data.sphj_list;
-      var sphj_id = [];
-      var sphj_name = [];
+      var sphj = [];
       if (splist.length > 1){
-        sphj_id.push('empty')
-        sphj_name.push('请选择审批环节')
+        sphj.push({ "id": "empty", "name": "请选择审批环节" })
         for (var i in splist) {
-          sphj_id.push(splist[i].sphj_id)
-          sphj_name.push(splist[i].sphj_name)
+          sphj.push({ "id": splist[i].sphj_id, "name": splist[i].sphj_name})
         }
         that.setData({
-          sphj_id: sphj_id,
-          sphj_name: sphj_name
+          sphj: sphj
         })
       }else{
-        sphj_id.push(splist[0].sphj_id)
-        sphj_name.push(splist[0].sphj_name);
+        sphj.push({ "id": splist[0].sphj_id, "name": splist[0].sphj_name })
         that.setData({
-          sphj_id: sphj_id,
-          sphj_name: sphj_name
+          sphj: sphj
         })
       }
       that.sphjChange(0)
@@ -70,43 +65,155 @@ Page({
     } else {
       var index = e
     }
-    var value = this.data.sphj_id[index]
+    var value = this.data.sphj[index].id
+    if (this.data.sphj_index != index){
+      this.setData({
+        spfs_index: 0,
+        ryfw_index: 0
+      })
+    }
     this.setData({
       sphj_index: index
     })
-    var spfs = this.getValueByKey('sphj_id', value, 'spfs_name', splist)
+    var spfsmcs = this.getValueByKey('sphj_id', value, 'spfs_name', splist)
     var spbmids = this.getValueByKey('sphj_id', value, 'sphj_depid', splist)
     var spbmmcs = this.getValueByKey('sphj_id', value, 'sphj_depname', splist)
     var spqzids = this.getValueByKey('sphj_id', value, 'sphj_groupid', splist)
     var spqzmcs = this.getValueByKey('sphj_id', value, 'sphj_groupname', splist)
     var spydyy = this.getValueByKey('sphj_id', value, 'sphj_person', splist)
-    if (app.isDefine(spfs)) {
-      var spfs_list = spfs.split(':')
-      var spfs_id = []
-      var spfs_name = []
+    var spfs = []
+    var ryfw = []
+    if (app.isDefine(spfsmcs)) {
+      var spfs_list = spfsmcs.split(':')
       if (spfs_list.length > 1) {
-        spfs_id.push('empty')
-        spfs_name.push('请选择审批方式')
+        spfs.push({"id": "empty", "name":"请选择审批方式"})
         for (var i in spfs_list) {
-          spfs_id.push(spfs_list[i])
-          spfs_name.push(spfs_list[i])
+          spfs.push({"id": spfs_list[i], "name": spfs_list[i]})
         }
         this.setData({
-          spfs_id: spfs_id,
-          spfs_name: spfs_name
+          spfs: spfs
         })
       }else{
-        spfs_id.push(spfs_list[0])
-        spfs_name.push(spfs_list[0]);
+        spfs.push({"id": spfs_list[0], "name": spfs_list[0]})
         this.setData({
-          spfs_id: spfs_id,
-          spfs_name: spfs_name
+          spfs: spfs
         })
       }
     }else{
       this.setData({
-        spfs_id: ['empty'],
-        spfs_name: ['请选择审批方式']
+        spfs: [{"id": "empty", "name": "请选择审批方式"}]
+      })
+    }
+    //人员范围只有部门选项
+    if (app.isDefine(spbmids) && !app.isDefine(spqzids) && !app.isDefine(spydyy)) {
+      spbmids = spbmids.split(':')
+      spbmmcs = spbmmcs.split(':')
+      if (spbmids.length > 1) {
+        ryfw.push({"id":"empty", "name":"请选择审批方式", "type":"N"})
+        for (var i in spbmids) {
+          ryfw.push({"id":spbmids[i], "name":spbmmcs[i], "type":"D"})
+        }
+        this.setData({
+          ryfw: ryfw
+        })
+      }else{
+        ryfw.push({"id":spbmids[0], "name":spbmmcs[0], "type":"D"})
+        this.setData({
+          ryfw: ryfw
+        })
+      }
+    }
+    //人员范围只有群组选项
+    if (!app.isDefine(spbmids) && app.isDefine(spqzids) && !app.isDefine(spydyy)) {
+      spqzids = spqzids.split(':')
+      spqzmcs = spqzmcs.split(':')
+      if (spqzids.length > 1) {
+        ryfw.push({ "id": "empty", "name": "请选择审批方式", "type": "N" })
+        for (var i in spqzids) {
+          ryfw.push({ "id": spqzids[i], "name": spqzmcs[i], "type": "G" })
+        }
+        this.setData({
+          ryfw: ryfw
+        })
+      } else {
+        ryfw.push({ "id": spqzids[0], "name": spqzmcs[0], "type": "G" })
+        this.setData({
+          ryfw: ryfw
+        })
+      }
+    }
+    //人员范围有部门和群组选项
+    if (app.isDefine(spbmids) && app.isDefine(spqzids) && !app.isDefine(spydyy)) {
+      spbmids = spbmids.split(':')
+      spbmmcs = spbmmcs.split(':')
+      spqzids = spqzids.split(':')
+      spqzmcs = spqzmcs.split(':')
+      ryfw.push({ "id": "empty", "name": "请选择审批方式", "type": "N" })
+      for (var i in spbmids) {
+        ryfw.push({ "id": spbmids[i], "name": spbmmcs[i], "type": "D" })
+      }
+      for (var i in spqzids) {
+        ryfw.push({ "id": spqzids[i], "name": spqzmcs[i], "type": "G" })
+      }
+      this.setData({
+        ryfw: ryfw
+      })
+    }
+    //人员范围有部门和预定人员
+    if (app.isDefine(spbmids) && !app.isDefine(spqzids) && app.isDefine(spydyy)) {
+      spbmids = spbmids.split(':')
+      spbmmcs = spbmmcs.split(':')
+      ryfw.push({ "id": "empty", "name": "请选择审批方式", "type": "N" })
+      ryfw.push({ "id": "0", "name": "系统预定人员", "type": "Y" })
+      for (var i in spbmids) {
+        ryfw.push({ "id": spbmids[i], "name": spbmmcs[i], "type": "D" })
+      }
+      this.setData({
+        ryfw: ryfw
+      })
+    }
+    //人员范围有群组和预定人员
+    if (!app.isDefine(spbmids) && app.isDefine(spqzids) && app.isDefine(spydyy)) {
+      spqzids = spqzids.split(':')
+      spqzmcs = spqzmcs.split(':')
+      ryfw.push({ "id": "empty", "name": "请选择审批方式", "type": "N" })
+      ryfw.push({ "id": "0", "name": "系统预定人员", "type": "Y" })
+      for (var i in spqzids) {
+        ryfw.push({ "id": spqzids[i], "name": spqzmcs[i], "type": "G" })
+      }
+      this.setData({
+        ryfw: ryfw
+      })
+    }
+    //人员范围有部门、群组和预定人员
+    if (app.isDefine(spbmids) && app.isDefine(spqzids) && app.isDefine(spydyy)) {
+      spbmids = spbmids.split(':')
+      spbmmcs = spbmmcs.split(':')
+      spqzids = spqzids.split(':')
+      spqzmcs = spqzmcs.split(':')
+      ryfw.push({ "id": "empty", "name": "请选择审批方式", "type": "N" })
+      ryfw.push({ "id": "0", "name": "系统预定人员", "type": "Y" })
+      for (var i in spbmids) {
+        ryfw.push({ "id": spbmids[i], "name": spbmmcs[i], "type": "D" })
+      }
+      for (var i in spqzids) {
+        ryfw.push({ "id": spqzids[i], "name": spqzmcs[i], "type": "G" })
+      }
+      this.setData({
+        ryfw: ryfw
+      })
+    }
+    //人员范围只有预定人员
+    if (!app.isDefine(spbmids) && !app.isDefine(spqzids) && app.isDefine(spydyy)) {
+      this.setData({
+        ryfw: [{ "id": "0", "name": "系统预定人员", "type": "Y" }]
+      })
+      this.ryfwChange(0)
+    }
+    //人员范围全部为空
+    if (!app.isDefine(spbmids) && !app.isDefine(spqzids) && !app.isDefine(spydyy)) {
+      this.setData({
+        ryfw: [{"id": "empty", "name": "请选择人员范围", "type": "N"}]
       })
     }
   },
@@ -114,6 +221,48 @@ Page({
     this.setData({
       spfs_index: e.detail.value
     })
+  },
+  ryfwChange: function (e) {
+    if (typeof e == 'object') {
+      var index = e.detail.value
+    } else {
+      var index = e
+    }
+    this.setData({
+      ryfw_index: index
+    })
+    var ryfw_type = this.data.ryfw[index].type
+    if (ryfw_type == 'Y') {
+      var sphj_id = this.data.sphj[this.data.sphj_index].id
+      var spydyy = this.getValueByKey('sphj_id', sphj_id, 'sphj_person', splist)
+      var person_list = []
+      var ydry_list = []
+      if (spydyy.indexOf(":") != -1) {
+        if (spydyy.substring(spydyy.length - 1, spydyy.length) == ":") {
+          spydyy = spydyy.substring(0, spydyy.length - 1);
+        }
+        ydry_list = spydyy.split(':');
+      }
+      if (ydry_list.length) {
+        person_list = ydry_list;
+      } else {
+        person_list.push(spydyy);
+      }
+      wx.setStorageSync("person_list", JSON.stringify(person_list))
+      this.addPerson('Y');
+      this.setData({
+        hasDefault: true
+      })
+    } else {
+      if (ryfw_type != 'N') {
+        this.setData({
+          hasDefault: false
+        })
+      }
+    }
+  },
+  addPerson: function (ryfw_type){
+    //TODO
   },
   getValueByKey: function (key, value, rekey, arr){
     if(arr.length) {
